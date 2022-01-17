2e5d06c9f18c10766931f48948f23758fabad4f6
MohammadBakhshi19 <77542557+MohammadBakhshi19@users.noreply.github.com>
2022-01-17 15:00:15 +0330

    initialize and add model folder

---
+++ Homework1/models/company.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const EmployeeSchema = require('./employee');
+const Schema = mongoose.Schema;
+const CompanySchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    registrationNumber: {
+        type: String,
+        required: true,
+    },
+    province: {
+        type: String,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    registrationDate: {
+        type: Date,
+        required: true
+    },
+    telephoneNumber: {
+        type: String,
+        required: true
+    },
+    employees:[EmployeeSchema],
+    timestamps:true
+})
