d5f4fb1d7a5d7b2d32af93421cbc2a7da73cbbdb
MohammadBakhshi19 <77542557+MohammadBakhshi19@users.noreply.github.com>
2022-01-23 19:16:03 +0330

    delete hw2

--- Homework2/config/database.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-
-const sequelize = new Sequelize('management', 'root', 'canyoudance78@#', {
-    host: 'localhost',
-    dialect: 'mysql'
-  });
-
-  module.exports =sequelize;
